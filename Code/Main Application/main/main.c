d93795573cbae3a631823748517b4f6bd65aba27
Mostafa Yehia <mustafayehia4@gmail.com>
2021-06-06 15:16:45 +0200

    Added Initial Code

---
+++ Code/Main Application/main/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ * Created: 6/6/2021 2:48:41 PM
+ * Author : Mustafa
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
