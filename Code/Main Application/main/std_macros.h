444d0d5d31a2300e8d89137c7fd6c248b2e2a596
Mostafa Yehia <mustafayehia4@gmail.com>
2021-06-06 17:53:00 +0200

    Edit Main Application

--- Code/Main Application/main/std_macros.h
+++ Code/Main Application/main/std_macros.h
@@ -1,10 +1,3 @@
-/*
- * std_macros.h
- *
- * Created: 6/6/2021 2:50:51 PM
- * Author : Mustafa Yehia
- */
-
 #define REGISTER_SIZE			8
 #define SET_BIT(reg, bit)		reg |= (1 << bit)
 #define CLR_BIT(reg, bit)		reg &= (~(1 << bit))
